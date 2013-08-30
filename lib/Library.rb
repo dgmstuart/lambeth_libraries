5c9c60c744c6c63f32f561cac54e52e71dd592f2
Duncan Stuart <dgmstuart@gmail.com>
2013-08-30 21:56:12 +0100

    squashing another bug: some hour listings used "-" instead of " to "

--- lib/Library.rb
+++ lib/Library.rb
@@ -41,14 +41,11 @@ class Library
   def build_opening_hours
     section_title = @library_page.at('h3:contains("Opening hours")')
     opening_hours_nodes = section_title.next.next.next.next.children
-    opening_hours_parts = opening_hours_nodes.map { |li| li.text.gsub(194.chr("UTF-8"),'').split(/[[:space:]]/) }
-    opening_hours_parts.inject({}) do |hash, array|
+    opening_hours_nodes.inject({}) do |hash, li |
+      cleaned_text = li.text.gsub(194.chr("UTF-8"),'').gsub("to",'').gsub("-",' ')
+      array = cleaned_text.split(/[[:space:]]+/)
       hash.update(array.first.downcase.to_sym => parse_hours(array.drop 1))
     end
-    # opening_hours_text = opening_hours_nodes.map { |li| li.text.gsub(/[[:space:]]/,' ').split(" -") }
-    # opening_hours_text.inject({}) do |hash, (day, hours)|
-    #   hash.update(day.downcase.to_sym => parse_hours_string(hours))
-    # end
   rescue ParseError => e
     raise "Couldn't parse the page content: #{e}"
   end
@@ -56,34 +53,25 @@ class Library
   def parse_hours(hours_array)
     raise ParseError, "Tried to parse a nil array of hours info" if hours_array.nil?
     raise ParseError, "Tried to parse an empty array of hours info" if hours_array.empty?
+    
+    # indexes in the array where we'd expect to find things:
+    opening_time = 0
+    closing_time = 1
     params = {}
-    if hours_array[0] == "-"
-      if hours_array[1].downcase == "closed"
-        params.update( closed: true )
-      
-      elsif hours_array[1][0] =~ /[0-9]/
-        # Assume it's a time
-        params.update( opening_time: hours_array[1])
-        if hours_array[2] == "to"
-          if hours_array[3].nil?
-binding.pry
-           raise ParseError, "Input ended after a 'to'" 
-          end
-          if hours_array[3][0] =~ /[0-9]/
-            params.update( closing_time: hours_array[3])
-          else
-            raise ParseError, "Found what looked like an opening time (\"#{hours_array[1]}\"), but the next item didn't look like a closing time (\"#{hours_array[3]}\")"
-          end
-        else
-          raise ParseError, "Found what looked like an opening time (\"#{hours_array[1]}\"), but this wasn't followed by \"to\""
-        end
-      
-      else # Assume it's a note which basically means it's closed
-        #binding.pry
-        params.update( closed: true )
+    if hours_array[0].downcase == "closed"
+      params.update( closed: true )
+    
+    elsif hours_array[opening_time][0] =~ /[0-9]/
+      # Assume it's a time
+      params.update( opening_time: hours_array[opening_time])
+
+      if hours_array[closing_time][0] =~ /[0-9]/
+        params.update( closing_time: hours_array[closing_time])
+      else
+        raise ParseError, "Found what looked like an opening time (\"#{hours_array[1]}\"), but the next item didn't look like a closing time (\"#{hours_array[3]}\")"
       end
-    else
-      #binding.pry
+    else # Assume it's a note which basically means it's closed
+      params.update( closed: true )
     end
 
     DayOpeningHours.new(params)
