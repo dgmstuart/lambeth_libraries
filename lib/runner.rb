3a92e989cea65081fcc3e3345676cb7d19b38d6a
Duncan Stuart <dgmstuart@gmail.com>
2013-08-31 03:38:19 +0100

    Added a command line interface

--- lib/runner.rb
+++ lib/runner.rb
@@ -1,26 +1,31 @@
-require './library'
+require_relative './library'
 
-BASE_URL = "http://www.lambeth.gov.uk"
-LIBRARY_PAGE_URL = "#{BASE_URL}/Services/LeisureCulture/Libraries/LocalLibraries/"
+module Runner
+  BASE_URL = "http://www.lambeth.gov.uk"
+  LIBRARY_PAGE_URL = "#{BASE_URL}/Services/LeisureCulture/Libraries/LocalLibraries/"
 
-def libraries
-  page = Nokogiri::HTML(open(LIBRARY_PAGE_URL))
-  names_and_urls = page.css("h2 a").map { |a| [a.text, a['href']] }
-  Hash[names_and_urls]
-end
+  def self.display_all
+    libraries.each_pair do | name, url |
+      puts Library.new(name, "#{BASE_URL}#{url}").display
+      puts 
+    end
+  end
 
-def display_all
-  libraries.each_pair do | name, url |
-    puts Library.new(name, "#{BASE_URL}#{url}").display
-    puts 
+  def self.display(library_name)
+    library = Library.new library_name, "#{BASE_URL}/Services/LeisureCulture/Libraries/LocalLibraries/#{library_name.delete(" ")}.htm"
+    puts library.display
+  end
+
+  def self.list
+    libraries.keys
+  end
+   
+  private 
+  
+  def self.libraries
+    page = Nokogiri::HTML(open(LIBRARY_PAGE_URL))
+    names_and_urls = page.css("h2 a").map { |a| [a.text, a['href']] }
+    Hash[names_and_urls]
   end
 end
 
-def display(library_name)
-  library = Library.new library_name, "#{BASE_URL}/Services/LeisureCulture/Libraries/LocalLibraries/#{library_name.delete(" ")}.htm"
-  puts library.display
-end
-
-display_all
-
-# display("Brixton Library")
